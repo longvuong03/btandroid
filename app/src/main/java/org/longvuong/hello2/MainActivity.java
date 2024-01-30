cbcc428dee6c04f0c8ba932d3147e0dfe776edd7
LONGV\longk <longkg4@gmail.com>
2024-01-30 19:20:06 +0700

    bt events

---
+++ app/src/main/java/org/longvuong/hello2/MainActivity.java
@@ -0,0 +1,34 @@
+package org.longvuong.hello2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonToast;
+    private Button buttonAlert;
+    private Button buttonCustomDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+}
